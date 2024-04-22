07e9802cf4dcfc47334fe8f1e5d3eb6c386a16b7
petercarr26 <90708851+petercarr26@users.noreply.github.com>
2024-04-22 12:51:10 -0400

    dining hall data has all unique primary keys

--- dininghall_data.sql
+++ dininghall_data.sql
@@ -21,73 +21,179 @@ INSERT INTO studentMP VALUES
 INSERT INTO studentMP VALUES 
   ('Caroline','Smith', '08202003','18304678','21 a week','17','none');
 
-INSERT INTO stations VALUES ('Southwest Station', 'Dougherty Hall');
-INSERT INTO stations VALUES ('Southwest Station', 'The Court at Donahue');
-INSERT INTO stations VALUES ('Southwest Station', 'St. Marys Hall');
-INSERT INTO stations VALUES ('Delicatessen', 'The Court at Donahue');
-INSERT INTO stations VALUES ('Delicatessen', 'Dougherty Hall');
-INSERT INTO stations VALUES ('Delicatessen', 'St. Marys Hall');
-INSERT INTO stations VALUES ('Salad Station', 'St. Marys Hall');
-INSERT INTO stations VALUES ('Salad Station', 'Dougherty Hall');
-INSERT INTO stations VALUES ('Salad Station', 'The Court at Donahue');
-INSERT INTO stations VALUES ('Pizza Station', 'The Court at Donahue');
-INSERT INTO stations VALUES ('Pizza Station', 'Dougherty Hall');
-INSERT INTO stations VALUES ('Holy Grounds', 'The Connelly Center');
-INSERT INTO stations VALUES ('Holy Grounds', 'Falvey Library');
-INSERT INTO stations VALUES ('Holy Grounds', 'Bartley Hall');
-INSERT INTO stations VALUES ('Delicatessen', 'Bartley Hall');
+INSERT INTO stations VALUES ('Southwest Main', 'Dougherty Hall');
+INSERT INTO stations VALUES ('Southwest South', 'The Court at Donahue');
+INSERT INTO stations VALUES ('Southwest West', 'St. Marys Hall');
+INSERT INTO stations VALUES ('Delicatessen South', 'The Court at Donahue');
+INSERT INTO stations VALUES ('Delicatessen Main', 'Dougherty Hall');
+INSERT INTO stations VALUES ('Delicatessen West', 'St. Marys Hall');
+INSERT INTO stations VALUES ('Salad West', 'St. Marys Hall');
+INSERT INTO stations VALUES ('Salad Main', 'Dougherty Hall');
+INSERT INTO stations VALUES ('Salad South', 'The Court at Donahue');
+INSERT INTO stations VALUES ('Pizza South', 'The Court at Donahue');
+INSERT INTO stations VALUES ('Pizza Main', 'Dougherty Hall');
+INSERT INTO stations VALUES ('Holy Grounds C', 'The Connelly Center');
+INSERT INTO stations VALUES ('Holy Grounds F', 'Falvey Library');
+INSERT INTO stations VALUES ('Holy Grounds B', 'Bartley Hall');
+INSERT INTO stations VALUES ('Delicatessen B', 'Bartley Hall');
 
 INSERT INTO menu VALUES
-('Monday', 'Salad Station', 'Lunch', 'various salad assortments');
+('Monday', 'Salad Main', 'Lunch', 'various salad assortments and spinach');
 INSERT INTO menu VALUES
-('Tuesday', 'Salad Station', 'Lunch', 'various salad assortments');
+('Tuesday', 'Salad Main', 'Lunch', 'various salad assortments and pepper');
 INSERT INTO menu VALUES
-('Wednesday', 'Salad Station', 'Lunch', 'various salad assortments');
+('Wednesday', 'Salad Main', 'Lunch', 'various salad assortments and coleslaw');
 INSERT INTO menu VALUES
-('Thursdsay', 'Salad Station', 'Lunch', 'various salad assortments');
+('Thursdsay', 'Salad Main', 'Lunch', 'various salad assortments and fruit');
 INSERT INTO menu VALUES
-('Friday', 'Salad Station', 'Lunch', 'various salad assortments');
+('Friday', 'Salad Main', 'Lunch', 'various salad assortments fried onion');
+
 INSERT INTO menu VALUES
-('Monday', 'Southwest Station', 'Dinner', 'rice, beans, meat, toppings');
+('Monday', 'Salad West', 'Lunch', 'various salad assortments and arugula');
 INSERT INTO menu VALUES
-('Tuesday', 'Southwest Station', 'Lunch', 'rice, beans, meat, toppings');
+('Tuesday', 'Salad West', 'Lunch', 'various salad assortments and pineapple');
 INSERT INTO menu VALUES
-('Wednesday', 'Southwest Station', 'Dinner', 'rice, beans, meat, toppings');
+('Wednesday', 'Salad West', 'Lunch', 'various salad assortments and french dressing');
 INSERT INTO menu VALUES
-('Thursdsay', 'Southwest Station', 'Lunch', 'rice, beans, meat, toppings');
+('Thursdsay', 'Salad West', 'Lunch', 'various salad assortments and orange slices');
 INSERT INTO menu VALUES
-('Friday', 'Southwest Station', 'Dinner', 'rice, beans, meat, toppings');
+('Friday', 'Salad West', 'Lunch', 'various salad assortments and hot pepper');
+
 INSERT INTO menu VALUES
-('Monday', 'Holy Grounds', 'Breakfast', 'egg nova');
+('Monday', 'Southwest Main', 'Dinner', 'rice, beans, beef, guac');
 INSERT INTO menu VALUES
-('Tuesday', 'Holy Grounds', 'Breakfast', 'egg nova');
+('Tuesday', 'Southwest Main', 'Lunch', 'rice, beans, steak, toppings');
 INSERT INTO menu VALUES
-('Wednesday', 'Holy Grounds', 'Breakfast', 'egg nova');
+('Wednesday', 'Southwest Main', 'Dinner', 'rice, beans, chicken, toppings');
 INSERT INTO menu VALUES
-('Thursdsay', 'Holy Grounds', 'Breakfast', 'egg nova');
+('Thursdsay', 'Southwest Main', 'Lunch', 'rice, beans, tofu, toppings');
 INSERT INTO menu VALUES
-('Friday', 'Holy Grounds', 'Breakfast', 'egg nova');
+('Friday', 'Southwest Main', 'Dinner', 'rice, beans, pork, toppings');
+
+INSERT INTO menu VALUES
+('Monday', 'Southwest South', 'Dinner', 'rice, black beans, beef, toppings');
+INSERT INTO menu VALUES
+('Tuesday', 'Southwest South', 'Lunch', 'rice, brown beans, steak, toppings');
+INSERT INTO menu VALUES
+('Wednesday', 'Southwest South', 'Dinner', 'rice, beans, birria, toppings');
+INSERT INTO menu VALUES
+('Thursdsay', 'Southwest South', 'Lunch', 'couscous, beans, chicken, toppings');
+INSERT INTO menu VALUES
+('Friday', 'Southwest South', 'Dinner', 'rice, beans, pork, toppings');
+
+INSERT INTO menu VALUES
+('Monday', 'Southwest West', 'Dinner', 'chicken fajitas');
+INSERT INTO menu VALUES
+('Tuesday', 'Southwest West', 'Lunch', 'birria quesadilla');
+INSERT INTO menu VALUES
+('Wednesday', 'Southwest West', 'Dinner', 'chicken quesadilla');
+INSERT INTO menu VALUES
+('Thursdsay', 'Southwest West', 'Lunch', 'beef tacos');
+INSERT INTO menu VALUES
+('Friday', 'Southwest West', 'Dinner', 'pork tacos');
 
 
-INSERT INTO inventory VALUES ('Southwest Station', 'grilled chicken', 'Contains none');
-INSERT INTO inventory VALUES ('Southwest Station', 'shredded cheese', 'Contains dairy');
-INSERT INTO inventory VALUES ('Southwest Station', 'diced tomatoes', 'Contains none');
-INSERT INTO inventory VALUES ('Southwest Station', 'black beans', 'Contains none');
-INSERT INTO inventory VALUES ('Delicatessen', 'ham', 'Contains none');
-INSERT INTO inventory VALUES ('Delicatessen', 'american cheese', 'Contains dairy');
-INSERT INTO inventory VALUES ('Delicatessen', 'tomatos', 'Contains none');
-INSERT INTO inventory VALUES ('Delicatessen', 'turkey', 'Contains none');
-INSERT INTO inventory VALUES ('Delicatessen', 'hoagie rolls', 'Contains gluten');
-INSERT INTO inventory VALUES ('Delicatessen', 'pickles', 'Contains none');
-INSERT INTO inventory VALUES ('Holy Grounds', 'black coffee', 'Contains none');
-INSERT INTO inventory VALUES ('Holy Grounds', 'whole milk', 'Contains dairy');
-INSERT INTO inventory VALUES ('Holy Grounds', 'bagels', 'Contains gluten');
-INSERT INTO inventory VALUES ('Holy Grounds', 'cream cheese', 'Contains dairy');
-INSERT INTO inventory VALUES ('Holy Grounds', 'black tea', 'Contains none');
-INSERT INTO inventory VALUES ('Pizza Station', 'red sauce', 'Contains none');
-INSERT INTO inventory VALUES ('Pizza Station', 'pizza dough', 'Contains gluten');
-INSERT INTO inventory VALUES ('Pizza Station', 'flour', 'Contains none');
-INSERT INTO inventory VALUES ('Pizza Station', 'mozzerella cheese', 'Contains diary');
+INSERT INTO menu VALUES
+('Monday', 'Holy Grounds F', 'Breakfast', 'sausage egg nova');
+INSERT INTO menu VALUES
+('Tuesday', 'Holy Grounds F', 'Breakfast', 'bacon egg nova');
+INSERT INTO menu VALUES
+('Wednesday', 'Holy Grounds F', 'Breakfast', 'egg nova plain');
+INSERT INTO menu VALUES
+('Thursdsay', 'Holy Grounds F', 'Breakfast', 'salmon egg nova');
+INSERT INTO menu VALUES
+('Friday', 'Holy Grounds F', 'Breakfast', 'bagel egg nova');
+
+INSERT INTO menu VALUES
+('Monday', 'Holy Grounds C', 'Breakfast', 'muffin, fruit');
+INSERT INTO menu VALUES
+('Tuesday', 'Holy Grounds C', 'Breakfast', 'fruit');
+INSERT INTO menu VALUES
+('Wednesday', 'Holy Grounds C', 'Breakfast', 'bagels');
+INSERT INTO menu VALUES
+('Thursdsay', 'Holy Grounds C', 'Breakfast', 'bacon egg nova, fruit');
+INSERT INTO menu VALUES
+('Friday', 'Holy Grounds C', 'Breakfast', 'muffin, sausage egg nova, fruit');
+
+INSERT INTO menu VALUES
+('Monday', 'Holy Grounds F', 'Breakfast', 'crossaint');
+INSERT INTO menu VALUES
+('Tuesday', 'Holy Grounds F', 'Breakfast', 'english muffin egg nova');
+INSERT INTO menu VALUES
+('Wednesday', 'Holy Grounds F', 'Breakfast', 'mango');
+INSERT INTO menu VALUES
+('Thursdsay', 'Holy Grounds F', 'Breakfast', 'bananas, muffins');
+INSERT INTO menu VALUES
+('Friday', 'Holy Grounds F', 'Breakfast', 'bacon egg and cheese');
+
+
+
+INSERT INTO inventory VALUES ('Southwest South', 'brown rice', 'none');
+INSERT INTO inventory VALUES ('Southwest South', 'shredded cheese', 'dairy');
+INSERT INTO inventory VALUES ('Southwest South', 'diced tomatoes', 'none');
+INSERT INTO inventory VALUES ('Southwest South', 'black beans', 'none');
+
+
+INSERT INTO inventory VALUES ('Southwest Main', 'white rice', 'none');
+INSERT INTO inventory VALUES ('Southwest Main', 'grated cheese', 'dairy');
+INSERT INTO inventory VALUES ('Southwest Main', 'sliced tomatoes', 'none');
+INSERT INTO inventory VALUES ('Southwest Main', 'black beans', 'none');
+
+INSERT INTO inventory VALUES ('Southwest West', 'chicken fajitas', 'none');
+INSERT INTO inventory VALUES ('Southwest West', 'birria quesadilla', 'dairy');
+INSERT INTO inventory VALUES ('Southwest West', 'chiken tacos', 'dairy');
+INSERT INTO inventory VALUES ('Southwest West', 'pork tacos', 'none');
+
+
+INSERT INTO inventory VALUES ('Delicatessen South', 'bologna', 'none');
+INSERT INTO inventory VALUES ('Delicatessen South', 'parmesean cheese', 'dairy');
+INSERT INTO inventory VALUES ('Delicatessen South', 'heirloom tomatos', 'none');
+INSERT INTO inventory VALUES ('Delicatessen South', 'salami', 'none');
+INSERT INTO inventory VALUES ('Delicatessen South', 'wheat wraps', 'gluten');
+INSERT INTO inventory VALUES ('Delicatessen South', 'dill pickles', 'none');
+
+INSERT INTO inventory VALUES ('Delicatessen Main', 'ham', 'none');
+INSERT INTO inventory VALUES ('Delicatessen Main', 'american cheese', 'dairy');
+INSERT INTO inventory VALUES ('Delicatessen Main', 'tomatos', 'none');
+INSERT INTO inventory VALUES ('Delicatessen Main', 'turkey', 'none');
+INSERT INTO inventory VALUES ('Delicatessen Main', 'hoagie rolls', 'gluten');
+INSERT INTO inventory VALUES ('Delicatessen Main', 'pickles', 'none');
+
+INSERT INTO inventory VALUES ('Delicatessen West', 'gabagool', 'none');
+INSERT INTO inventory VALUES ('Delicatessen West', 'provolone cheese', 'dairy');
+INSERT INTO inventory VALUES ('Delicatessen West', 'grape tomatos', 'none');
+INSERT INTO inventory VALUES ('Delicatessen West', 'roasted turkey', 'none');
+INSERT INTO inventory VALUES ('Delicatessen West', 'luscios rolls', 'gluten');
+INSERT INTO inventory VALUES ('Delicatessen West', 'pickled onions', 'none');
+
+
+INSERT INTO inventory VALUES ('Holy Grounds C', 'colombian black coffee', 'none');
+INSERT INTO inventory VALUES ('Holy Grounds C', '2% milk', 'dairy');
+INSERT INTO inventory VALUES ('Holy Grounds C', 'everything bagels', 'gluten');
+INSERT INTO inventory VALUES ('Holy Grounds C', 'strawberry cream cheese', 'dairy');
+INSERT INTO inventory VALUES ('Holy Grounds C', 'black tea', 'none');
+
+INSERT INTO inventory VALUES ('Holy Grounds B', 'french black coffee', 'none');
+INSERT INTO inventory VALUES ('Holy Grounds B', 'whole milk', 'dairy');
+INSERT INTO inventory VALUES ('Holy Grounds B', 'egg bagels', 'gluten');
+INSERT INTO inventory VALUES ('Holy Grounds B', 'plain cream cheese', 'dairy');
+INSERT INTO inventory VALUES ('Holy Grounds B', 'green tea', 'none');
+
+INSERT INTO inventory VALUES ('Holy Grounds F', 'black coffee', 'none');
+INSERT INTO inventory VALUES ('Holy Grounds F', 'skim milk', 'dairy');
+INSERT INTO inventory VALUES ('Holy Grounds F', 'plain bagels', 'gluten');
+INSERT INTO inventory VALUES ('Holy Grounds F', 'matcha mix', 'dairy');
+INSERT INTO inventory VALUES ('Holy Grounds F', 'herbal tea', 'none');
+
+
+INSERT INTO inventory VALUES ('Pizza South ', 'red sauce', 'none');
+INSERT INTO inventory VALUES ('Pizza South', 'flour', 'gluten');
+INSERT INTO inventory VALUES ('Pizza South', 'mozzerella cheese', 'dairy');
+
+INSERT INTO inventory VALUES ('Pizza Main', 'vodka sauce', 'dairy');
+INSERT INTO inventory VALUES ('Pizza Main', 'pizza dough', 'gluten');
+INSERT INTO inventory VALUES ('Pizza Main', 'gluten free flour', 'none');
+INSERT INTO inventory VALUES ('Pizza Main', 'fat free mozzerella cheese', 'dairy');
 
 INSERT INTO restrictions VALUES ('02405999', 'none', 'The Court at Donahue', 'none');
 INSERT INTO restrictions VALUES ('02416009', 'shellfish', 'St. Marys Hall', 'gluten');
@@ -100,3 +206,4 @@ INSERT INTO restrictions VALUES ('84653849', 'shellfish', 'St. Marys Hall', 'non
 INSERT INTO restrictions VALUES ('98763098', 'none', 'The Court at Donahue', 'none');
 INSERT INTO restrictions VALUES ('17284067', 'none', 'Bartley hall', 'gluten');
 INSERT INTO restrictions VALUES ('18304678', 'none', 'St. Marys Hall', 'dairy');
+
